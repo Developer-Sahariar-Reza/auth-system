19eaa8005495560677038ea7cac1f639aa09cdeb
Developer-Sahariar-Reza <dev.sahariar.reza@gmail.com>
2023-09-17 16:41:34 +0600

    left nav category data fetch done

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./LeftNav.css";
+import { Link } from "react-router-dom";
 
 const LeftNav = () => {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data))
+      .catch((error) => console.error(error));
+  }, []);
   return (
     <div>
-      <h1>Left Nav</h1>
+      <h1 className="left-nav-title">All Categories</h1>
+      {categories.map((category) => (
+        <li className="category-name" key={category.id}>
+          <Link to={`/category/${category.id}`}>{category.name}</Link>
+        </li>
+      ))}
     </div>
   );
 };
