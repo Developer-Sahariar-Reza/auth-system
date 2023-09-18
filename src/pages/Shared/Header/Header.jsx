1e5b21d61b2de57d21fe624f22a90850e48fca1c
Developer-Sahariar-Reza <dev.sahariar.reza@gmail.com>
2023-09-18 18:28:42 +0600

    slogan, time and marquee added

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Header.css";
 import logo from "../../../assets/icon/logo.svg";
 import NavigationMenu from "../NavigationMenu/NavigationMenu";
+import moment from "moment";
 import Marquee from "react-fast-marquee";
 
 const Header = () => {
@@ -24,8 +25,33 @@ const Header = () => {
         <div className="slogan">
           <h3>The News Bengal: Your Source, Your Voice!</h3>
         </div>
-        <div className="time"></div>
-        <div className="breaking-news"></div>
+
+        <div className="time">
+          <h4>Today: {moment().format("dddd, MMMM Do, YYYY")}</h4>
+        </div>
+
+        <div className="breaking-news-container">
+          <h4>Latest</h4>
+          <Marquee
+            className="breaking-newses"
+            speed={100}
+            gradient={true}
+            pauseOnHover={true}
+          >
+            <p>
+              Dengue deaths, patients on the rise as local govt can't control!!!
+            </p>
+
+            <p>
+              China flies 103 military planes toward Taiwan in a new high of
+              activity!!!
+            </p>
+
+            <p>Dhaka Elevated Expressway: BRTC bus service kicks off!!!</p>
+
+            <p>EU fines TikTok 345 million euros over child data!!!</p>
+          </Marquee>
+        </div>
       </section>
     </header>
   );
