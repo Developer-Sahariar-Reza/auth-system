cb310badece807433484ca02fabcad6ea051df42
Developer-Sahariar-Reza <dev.sahariar.reza@gmail.com>
2023-09-22 13:49:42 +0600

    news details added

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import "./News.css";
+import { Link, useLoaderData } from "react-router-dom";
+import { FaArrowLeftLong } from "react-icons/fa6";
 
 const News = () => {
-  return <div></div>;
+  const news = useLoaderData();
+  const { _id, image_url, category_id, title, details } = news;
+  return (
+    <div className="news-details-container">
+      <div className="news-details-image">
+        <img src={image_url} alt="" />
+      </div>
+      <div className="news-info">
+        <h1>{title}</h1>
+        <p>{details}</p>
+        <Link to={`/category/${category_id}`}>
+          <button className="common-button">
+            <FaArrowLeftLong /> <span>All News in this Category</span>
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default News;
