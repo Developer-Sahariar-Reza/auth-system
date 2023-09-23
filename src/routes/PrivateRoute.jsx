23986bf3f72432ecb38f654d942282279e56e88c
Developer-Sahariar-Reza <dev.sahariar.reza@gmail.com>
2023-09-23 14:34:47 +0600

    loading fix

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -4,8 +4,11 @@ import { AuthContext } from "../providers/AuthProvider";
 import { Navigate, useLocation } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
-  const { user } = useContext(AuthContext);
+  const { user, loading } = useContext(AuthContext);
   const location = useLocation();
+  if (loading) {
+    return "Loading.....";
+  }
   if (user) {
     return children;
   }
